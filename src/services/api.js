08fcd66fd3f06640a199cbb0602509dabfb8b839
yurimoura <yurimoura9626@outlook.com>
2024-04-26 19:19:18 -0300

    add da url do backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://giant-fox-undershirt.cyclic.app',
     timeout: 30000,
     headers: { 'Content-Type': 'application/json' }
 });
